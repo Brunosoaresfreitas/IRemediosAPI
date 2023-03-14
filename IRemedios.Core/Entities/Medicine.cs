d90ee13a51c183adcaa7e1b17eac1f4bafe8a09d
Bruno Henrique <brunosoaresfreitas26@gmail.com>
2023-03-13 21:01:31 -0300

    Atualizando

--- IRemedios.Core/Entities/Medicine.cs
+++ IRemedios.Core/Entities/Medicine.cs
@@ -1,4 +1,6 @@
-﻿namespace IRemedios.Core.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IRemedios.Core.Entities
 {
     public class Medicine
     {
@@ -13,10 +15,18 @@
             PostedAt = DateTime.Now;
         }
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required!")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "{0} should have between {2} and {1} caracters!")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "{0} is required!")]
+        [StringLength(200, ErrorMessage = "{0} can't exceed {1} caracters!")]
         public string Description { get; set; }
+        [Required(ErrorMessage = "{0} is required!")]
         public string Manufacturer { get; set; }
+        [Required(ErrorMessage = "{0} is required!")]
         public string DosageInstructions { get; set; }
+        [Required(ErrorMessage = "{0} is required!")]
         public string Warnings { get; set; }
         public DateTime PostedAt { get; set; }
     }
